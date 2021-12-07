4ccd1b328633a8bc0752e52f5aeaadf983cc106b
Imanuel Ulbricht <me@imanuel.dev>
2021-12-07 20:30:53 +0100

    Added content length header

--- http/fonts.go
+++ http/fonts.go
@@ -5,6 +5,7 @@ import (
 	"jinya-fonts/config"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,10 +19,17 @@ func GetFont(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	filestat, err := file.Stat()
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Content-Type", "font/woff2")
+	w.Header().Set("Content-Length", strconv.Itoa(int(filestat.Size())))
 	_, err = io.Copy(w, file)
 	if err != nil {
 		http.NotFound(w, r)
