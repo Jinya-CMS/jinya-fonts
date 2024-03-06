0f2d45cb385fac1ba003750dd4837b74ad25b922
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-03-06 01:16:57 +0100

    TG-24 Rewrite frontend in Angular
    Added kind of lazy loading for displaying of fonts

--- angular/frontend/src/app/startpage/startpage.component.ts
+++ angular/frontend/src/app/startpage/startpage.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ApiModule } from '../api/api.module';
 import { ApiService } from '../api/services/api.service';
 import { previewTexts, SettingsPanelComponent, WebfontFilter } from '../settings-panel/settings-panel.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Webfont } from '../api/models/webfont';
 import { FontCardComponent } from '../font-card/font-card.component';
+
+interface WebfontWithRow extends Webfont {
+  rowStart: number;
+  rowEnd: number;
+  rowStartFull: number;
+  rowEndFull: number;
+}
 
 @Component({
   selector: 'app-startpage',
@@ -17,16 +24,28 @@ export class StartpageComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
   webfonts: Webfont[] = [];
-  filteredWebfonts: Webfont[] = [];
+  filteredWebfonts: WebfontWithRow[] = [];
+  fontsToShow: WebfontWithRow[] = [];
   loading = true;
   text = previewTexts.lorem;
   size = 24;
+  pageSize = Math.floor(window.innerHeight / (13 * 16));
+
+  protected readonly Math = Math;
 
   ngOnInit(): void {
     this.apiService.apiFontGet().subscribe((value) => {
       this.webfonts = value;
-      this.filteredWebfonts = value;
       this.loading = false;
+      this.filter({
+        display: true,
+        handwriting: true,
+        monospace: true,
+        sansSerif: true,
+        searchText: '',
+        serif: true
+      });
+      this.setFontsToShow();
     });
   }
 
@@ -52,6 +71,33 @@ export class StartpageComponent implements OnInit {
         }
 
         return result;
-      });
+      })
+      .map((font, idx) => ({
+        ...font,
+        rowStart: Math.floor((idx === 0 ? 0 : idx / 4) + 1),
+        rowEnd: Math.floor((idx === 0 ? 0 : idx / 4) + 2),
+        rowStartFull: idx + 1,
+        rowEndFull: idx + 2
+      }));
+    this.setFontsToShow();
+    window.scrollTo({
+      top: 0
+    });
+  }
+
+  setFontsToShow() {
+    let page = Math.floor((window.scrollY + window.innerHeight) / window.innerHeight) - 1;
+    if (page < 0) {
+      page = 0;
+    }
+
+    const startIndex = page * this.pageSize;
+    const endIndex = startIndex * 4 + this.pageSize * 4 * 2 * 2;
+    this.fontsToShow = this.filteredWebfonts.slice(startIndex * 4, endIndex);
+  }
+
+  @HostListener('window:scroll')
+  windowScroll() {
+    this.setFontsToShow();
   }
 }
